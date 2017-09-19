81c9c99b1fab6328e341bf5905c1069adc2f3b49
Nathan M. Bivens <nathanmbivens@gmail.com>
2017-09-18 21:06:06 -0500

    Finished Navigation
    Navigation bar styles are done. Moving onto the jumbotron.

--- src/Components/HOME/Navigation/Navigation.js
+++ src/Components/HOME/Navigation/Navigation.js
@@ -4,7 +4,8 @@ export default class Navigation extends React.Component {
   render() {
     return (
       <div>
-        <div className="navigation-bar"> Navigation
+        <div className="navigation-bar">
+          <div className="navigation-title"> SPORTA </div>
           <div className="nav">
             <div className="nav-item">About</div>
             <div className="nav-item">Therapy</div>
